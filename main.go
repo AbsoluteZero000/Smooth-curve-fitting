0413f83bf6cc37e31d1f3792b6aa47332a56f90d
Mostafa Ahmed <m.a7med82002@gmail.com>
2023-11-25 19:39:16 +0200

    printing output to a file

--- main.go
+++ main.go
@@ -219,6 +219,37 @@ func replacement(generation [][]float64, copySize int, points [][]float64, offSp
 	return selected
 }
 
+func WriteBestIndividualToFile(fileName string, dataSetIndex int, coefficients []float64, mse float64) error {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "Data Set Index: %d\n", dataSetIndex)
+	if err != nil {
+		return err
+	}
+
+	for i := len(coefficients) - 1; i > 0; i-- {
+		_, err := fmt.Fprintf(file, "%.6f x^%d + ", coefficients[i], i)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprintf(file, "%.6f\n", coefficients[0])
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(file, "Mean Square Error: %.6f\n", mse)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // //////////////////////// Main Function ////////////////////////////
 func start() {
 	maxGeneration := 1000
@@ -268,22 +299,25 @@ func start() {
 			mutatedPool := mutation(crossedOverPool, lowerBound, upperBound, i, maxGeneration)
 			population = replacement(population, copiedParentsSize, points, mutatedPool)
 		}
-		fmt.Println("DATA SET", i+1, "\nBest Individual: ")
-		for j := degree; j > 0; j-- {
-			fmt.Print(population[0][j], " x^", j, " ", "+ ", " ")
+		individual := bestIndividual(population, points)
+		totalError := 0.0
+		N := float64(len(points))
+
+		for _, point := range points {
+			x := float64(point[0])
+			yActual := float64(point[1])
+			yCalc := calculatePolynomial(x, individual)
+			error := math.Pow(yCalc-yActual, 2)
+
+			totalError += error
 		}
-		fmt.Println(population[0][0])
-		fmt.Println("\n","cofficeints : ")
-		
-		for j := degree; j > 0; j-- {
-			fmt.Print(population[0][j]," "," ")
-		}
-		fmt.Println(population[0][0],"\n")
+
+		mse := totalError / N
+		WriteBestIndividualToFile("output.txt", i+1, individual, mse)
 	}
-	
-	
 
 }
+
 // /////////////////////////// Start ////////////////////////////////
 func main() {
 	rand.Seed(43)
